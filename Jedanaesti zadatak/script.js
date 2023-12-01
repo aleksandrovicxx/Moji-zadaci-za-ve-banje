50214a91d96b3824aaee25479146d1db50385685
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2023-12-01 23:12:03 +0100

    01.12.2023.

---
+++ Jedanaesti zadatak/script.js
@@ -0,0 +1,41 @@
+// 1 ZADATAK
+
+let a = 5;
+let b = 10;
+let c = 15;
+
+let zbir = a + b + c;
+console.log(zbir);
+let proizvod = a * b * c;
+console.log(proizvod);
+let ar_sredina = zbir / 3;
+console.log(ar_sredina);
+
+// 2 ZADATAK
+
+let strana_a = 5;
+let strana_b = 2;
+let kvadrat_strane_a = strana_a * strana_a;
+let kvadrat_strane_b = strana_b * strana_b;
+let zbir_kvadrata = kvadrat_strane_b + kvadrat_strane_a;
+let koren = Math.sqrt(zbir_kvadrata);
+console.log(koren);
+
+// 3 ZADATAK
+
+let ugao = 90;
+let radijani = (ugao * Math.PI) / 180;
+console.log(radijani)
+
+// OSTALE ZADATAKE TREBA NASTAVITI
+
+//ZADACI IZ .txt FAJLA
+
+// 1. ZADATAK
+let sati = 3;
+let minuti = 10;
+let sati_u_minutima = sati * 60;
+let nakon_ponoći = sati_u_minutima + minuti;
+console.log(nakon_ponoći);
+
+//SVE SAM PROVEŽBAO RANIJE
